d8c221dd6aad543ccce2e40ddec75ffe451065c2
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-09-11 15:44:34 +0530

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,33 +2,6 @@ import React, { useContext } from 'react'
 import {FaLaptop, FaTabletAlt, FaMobileAlt} from 'react-icons/fa'
 import dataContext from './context/dataContext'
 
-/*js destructuring obj
-const Header = ({title}) => {
- 
-  return (
-    <header>
-        <h1> {title} </h1> 
-    </header>
-  )
-}
-*/
-
-//jsx
-/*const Header = (props) => {
- 
-  return (
-    <header>
-        <h1> {props.title} </h1> 
-    </header>
-  )
-}
-
-Header.defaultProps = {
-  title: "To do List"
-}
-export default Header
-*/
-
 const Header = ({ title = "To do List" }) => {
   const {width} = useContext(dataContext)
   return (
