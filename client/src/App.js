e01d25e99613ac1d3a059353e9dd0579d8b2d958
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-09-11 15:44:06 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,3 @@
-/*
-import logo from './logo.svg';
-import './App.css';
-
-
-<header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<p> SUscribe to ggm </p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
-</header>
-*/
 //JSX - javascript & Xml (file)
 import Header from "./Header";
 import Footer from "./Footer";
@@ -31,15 +10,6 @@ import { Route, Routes } from "react-router-dom";
 import ItemsList from "./todoItemsList";
 import LineItem from "./LineItem";
 
-//<Header /> means Components
-
-//Props
-/*passing parameters in components is props like func "paras"
-
-//Props Drilling
-Drilling means info pass parent to child like "header to content to footer to header (link)"
-It can't be happen child to child resource sharing like "footer & Content => (This case possible when the resources are provided by parent then child & other child can mutually share their resources)" but parent to child is possible like "app & footer"
-*/
 function App() {
 
   return (
